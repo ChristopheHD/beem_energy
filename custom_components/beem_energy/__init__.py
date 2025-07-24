1cdc3fd38a8f549d561f32d4829b8851eef8646b
Christophe Hardouin Duparc <christophe.hardouin.duparc@gmail.com>
2025-07-24 16:04:42 +0200

    Update __init__.py

--- custom_components/beem_energy/__init__.py
+++ custom_components/beem_energy/__init__.py
@@ -4,8 +4,10 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.data.setdefault(DOMAIN, {})
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    hass.data.setdefault(DOMAIN, {})    
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
